0d609f8dfd655828aebf04f8bf15f07a3e0f647b
Aurora Murador <aurora.murador@gmail.com>
2022-06-16 17:59:50 +0200

    feat(login): add login page

--- app/root.tsx
+++ app/root.tsx
@@ -21,12 +21,12 @@ export const meta: MetaFunction = () => ({
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full bg-gray-50">
       <head>
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="h-full">
         <Outlet />
         <ScrollRestoration />
         <Scripts />
